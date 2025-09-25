a420b37b41796c79a347444b72903a8b1b03b2b5
Bugor <andreyvlasov03@ya.ru>
2025-09-25 09:10:34 +0300

    установил порт

--- oauth_server.py
+++ oauth_server.py
@@ -156,4 +156,5 @@ def is_auth():
     return jsonify({"authorized": bool(login), "github_login": login})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
